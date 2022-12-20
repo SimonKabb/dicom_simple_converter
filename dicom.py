8246988f22239267fb81edb50a3adfb4ff8832d2
Семён Капустников <s.v.kapustnikov@gmail.com>
2022-12-20 23:33:19 +0300

    ge dicoms dont convert

--- dicom.py
+++ dicom.py
@@ -3,12 +3,16 @@ import pathlib
 import cv2
 import numpy
 import pydicom
-from PIL import Image
+from PIL import Image, ImageEnhance
 from pydicom.errors import InvalidDicomError
+from sys import platform
 
 
 def make_filename(filename):
-    filename = 'videos/' + filename.split('/')[1]
+    if platform == 'win32':
+        filename = 'videos\\' + filename.split('\\')[1]
+    else:
+        filename = 'videos/' + filename.split('/')[1]
     return filename.split('.')[0] + '.avi'
 
 
@@ -20,7 +24,7 @@ def make_video(filename):
         return None
     array = ds.pixel_array
     video = cv2.VideoWriter(make_filename(filename), cv2.VideoWriter_fourcc(
-        'M', 'J', 'P', 'G'), 12, (512, 512))  # Initialize Video File
+        'M', 'J', 'P', 'G'), 12, (750, 750))  # Initialize Video File
     for frame in array:
         frame_rgb = cv2.cvtColor(frame, cv2.COLOR_GRAY2BGR)
         video.write(frame_rgb)
@@ -33,9 +37,21 @@ def make_photo(filename, frame):
     except InvalidDicomError:
         return None
     array = ds.pixel_array
-    array = numpy.reshape(array[35], (512, 512))
-    data = Image.fromarray(array)
+    array = numpy.reshape(array[20], (750, 750))
+    data = Image.fromarray(array).convert('RGB')
     data.save('gfg_dummy_pic.png')
+
+    im = Image.open('gfg_dummy_pic.png')
+    im = im.convert('LA')
+    # print(im.mode)
+    # enhancer = ImageEnhance.Brightness(im)
+    # im_output = enhancer.enhance(0.5)
+    # enhancer = ImageEnhance.Contrast(im)
+    # print(enhancer)
+    # im_output = enhancer.enhance(10)
+    # enhancer = ImageEnhance.Sharpness(im)
+    # im_output = enhancer.enhance(2)
+    im.save('gfg_dummy_pic1.png')
 
 
 def search_files():
